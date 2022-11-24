50f55069057d54f0ca18f157ffb2fe795d1425ab
J6-Babbage <edwinjpr11@gmail.com>
2022-11-24 17:33:41 -0500

    initial commit with shared, reactive, template modules

---
+++ src/app/shared/sidemenu/sidemenu.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sidemenu',
+  templateUrl: './sidemenu.component.html',
+  styles: [],
+})
+export class SidemenuComponent {}
